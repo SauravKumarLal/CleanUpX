7b7c1648a4bfd06639ebee7ab563fddf550b8d9f
SauravKumarLal <sauravkrlal@gmail.com>
2024-12-29 16:37:32 +0530

    done

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://neondb_owner:L0wVkxhogbl9@ep-lingering-mountain-a580r0hw.us-east-2.aws.neon.tech/neondb?sslmode=require",
+      connectionString:
+        "postgresql://neondb_owner:L0wVkxhogbl9@ep-lingering-mountain-a580r0hw.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    },
+  };
+  
